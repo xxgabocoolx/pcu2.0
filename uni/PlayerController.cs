dfc2fe2d3a8dac3501c5bd3baa8bd5b74423a174
gabo <gegm2006@gmail.com>
2020-05-03 15:14:36 -0500

    pcu2.0

---
+++ uni/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalMove;
+    public float verticalMove;
+    private Vector3 playerInput;
+
+    public CharacterController Player;
+
+    public float PlayerSpeed;
+    private Vector3 movePlayer;
+    public float gravity = 9.8f;
+    public float fallVelocity;
+    public float jumpForce;
+
+
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 camRight;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+        playerInput = new Vector3(horizontalMove, 0, verticalMove);
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDirection();
+
+        movePlayer = playerInput.x * camRight + playerInput.z * camForward;
+
+        movePlayer = movePlayer * PlayerSpeed;
+
+        Player.transform.LookAt(Player.transform.position + movePlayer);
+
+        setGravity();
+
+        PlayerSkills();
+
+        Player.Move(movePlayer * Time.deltaTime);
+
+        Debug.Log(Player.velocity.magnitude);
+    }
+
+    void camDirection()
+    {
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y = 0;
+        camRight.y = 0;
+
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+
+    }
+
+    public void PlayerSkills()
+    {
+        if (Player.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            fallVelocity = jumpForce;
+            movePlayer.y = fallVelocity;
+        }
+    }
+
+    void setGravity()
+    {
+        if (Player.isGrounded)
+        {
+            fallVelocity = -gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+        }
+        else
+        {
+           fallVelocity -= gravity * Time.deltaTime;
+            movePlayer.y = fallVelocity;
+
+        }
+    }
+
+
+} 
